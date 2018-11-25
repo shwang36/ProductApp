20b1cd5e6e5434f547fec6f05c5287901dcf561f
shwang36 <shwang36@gmail.com>
2018-11-25 16:17:00 +0800

    加入專案檔案。

---
+++ ProductApp/App_Start/WebApiConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace ProductApp
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API 設定和服務
+
+            // Web API 路由
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            config.Routes.MapHttpRoute(
+                name: "ActionApi",
+                routeTemplate: "api/{controller}/{action}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ ProductApp/Controllers/ProductsController.cs
@@ -0,0 +1,90 @@
+﻿using ProductApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+
+namespace ProductApp.Controllers
+{
+    public class ProductsController : ApiController
+    {
+
+        Product[] products = new Product[]
+        {
+            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
+            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
+            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+        };
+
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            if (products == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            return products;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        [HttpGet]
+        [ActionName("Thumbnail")]
+        public IHttpActionResult Details(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        [HttpDelete]
+        [ActionName("Thumbnail2")]
+        public void Delete(int id)
+        {
+
+        }
+
+
+        [HttpPut]
+        [ActionName("Thumbnail2")]
+        public void Put(int id, [FromBody]test json)
+        {
+            test b = json;
+            Console.WriteLine("");
+        }
+
+        [HttpPost]
+        [ActionName("Thumbnail2")]
+        public void Post(int id, [FromBody]test json)
+        {
+            test b = json;
+
+            string a = json.Category.ToString();
+            //     test t = JsonConvert.DeserializeObject<test>(json);
+            /*            JObject jo = JObject.Parse(json);
+
+                        string l_type = jo["123"].ToString();
+                        string param1 = jo["PARAM1"].ToString();
+            */
+            Console.WriteLine("");
+        }
+
+    }
+}
